90fc5280f1fb931e9637934444809b8b34d7622a
SuzukiKumpei <suzuki.kumpei@gmail.com>
2020-02-28 11:52:20 +0900

    first commit

---
+++ src/main/java/com/example/BbsGitPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsGitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsGitPracticeApplication.class, args);
+	}
+
+}
